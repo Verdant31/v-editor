48439aa0ae714e752360a16c193f4e417e5ccadc
verdant31 <joao.pedro3110@hotmail.com>
2023-02-26 23:12:19 -0400

    start to creating a resizable container for application page

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -1,11 +1,37 @@
+import interact from 'interactjs';
+import { Lightning } from 'phosphor-react';
+import './index.css';
 import { useProject } from './useProject';
 
 export function Project() {
   const { handleRunProject, output } = useProject();
-  
+  interact('.resizable')
+  .resizable({
+    edges: { top: true, left: true, bottom: true, right: true },
+    listeners: {
+      move: function (event) {
+        let { x, y } = event.target.dataset
+
+        x = (parseFloat(x) || 0) + event.deltaRect.left
+        y = (parseFloat(y) || 0) + event.deltaRect.top
+
+        Object.assign(event.target.style, {
+          width: `${event.rect.width}px`,
+          height: `${event.rect.height}px`,
+          transform: `translate(${x}px, ${y}px)`
+        })
+
+        Object.assign(event.target.dataset, { x, y })
+      }
+    }
+  })
+
   return (
-    <main className="flex justify-between">
-      <button onClick={handleRunProject}> Run react project</button>
+    <main className="flex  px-24">
+      <button className="flex gap-2 justify-center items-center absolute top-10 right-10 h-10 bg-[#8257e5] rounded-md w-24 hover:scale-105 toransition-all duration-300" onClick={handleRunProject}> 
+        <p className="text-lg">Run</p>
+        <Lightning size={24} weight="fill" />
+      </button>
       {output.length > 0 &&  (
         <div className="font-monospace text-xs leading-loose mt-12">
           {output.map((line) => {
@@ -13,7 +39,7 @@ export function Project() {
           })}
         </div>
       )}
-      <iframe src="" width={800} height={800}  />
+      <iframe className="resizable" style={{boxSizing: 'border-box'}} width="100%" height="100%"  src="" />
     </main>
   )
 }

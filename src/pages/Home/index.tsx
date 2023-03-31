2a9634b7b2546e60e6cfd5b7568dd5581774b11a
verdant31 <joao.pedro3110@hotmail.com>
2023-03-31 12:07:09 -0400

    removing toastify

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,6 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { Title } from './components/Title';
 
 export default function Home() {
